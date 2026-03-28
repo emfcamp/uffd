814bf826a1e697480b3e3766a710aa4b9bec3149
Luke Granger-Brown <git@lukegb.com>
2026-03-28 00:23:18 +0000

    Merge two alembic head revisions together

--- uffd/migrations/versions/b9839ad4fd60_remove_mfa_done_from_session.py
+++ uffd/migrations/versions/b9839ad4fd60_remove_mfa_done_from_session.py
@@ -14,7 +14,7 @@ branch_labels = None
 depends_on = None
 
 def upgrade():
-	meta = sa.MetaData(bind=op.get_bind())
+	meta = sa.MetaData()
 	session = sa.Table('session', meta,
 		sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
 		sa.Column('secret', sa.Text(), nullable=True),
@@ -32,7 +32,7 @@ def upgrade():
 		batch_op.drop_column('mfa_done')
 
 def downgrade():
-	meta = sa.MetaData(bind=op.get_bind())
+	meta = sa.MetaData()
 	with op.batch_alter_table('session') as batch_op:
 		batch_op.add_column(sa.Column('mfa_done', sa.Boolean(create_constraint=True, name=op.f('ck_session_mfa_done')), nullable=False, server_default=sa.true()))
 	session = sa.Table('session', meta,
---
+++ uffd/migrations/versions/cc085bf551ae_merge_heads.py
@@ -0,0 +1,24 @@
+"""Merge heads
+
+Revision ID: cc085bf551ae
+Revises: 176473f0a225, b9839ad4fd60
+Create Date: 2026-03-28 00:43:37.373221
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cc085bf551ae'
+down_revision = ('176473f0a225', 'b9839ad4fd60')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
